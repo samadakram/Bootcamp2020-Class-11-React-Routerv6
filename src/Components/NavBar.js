224d7cd55d9fda5aa3f35a9d0a670f7465f123c3
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-08 03:19:35 +0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
-            <h1>NavBar</h1>
+            <Link to="">Home</Link>
         </div>
     );
 }
