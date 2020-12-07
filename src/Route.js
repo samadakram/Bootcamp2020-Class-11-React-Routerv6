d58543dd7194f57c92e1e41207de8f4e8460215c
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-08 02:41:14 +0500

    Update Route.js

--- src/Route.js
+++ src/Route.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { BrowerRouter as Router, Switch, Route } from 'react-router-dom';
 
 const RouteConfig = () => {
     return (
-        <div></div>
+        <div>
+            <Router>
+                <Switch>
+                    <Route path="/" component={} />
+                </Switch>
+            </Router>
+        </div>
     );
 }
 
