873c1be055e1217a0abedc7651eb4c15ab742537
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-08 02:49:07 +0500

    Create ProductItem.js

---
+++ src/Components/ProductItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductItem = () => {
+    return (
+        <div>
+            <h1>Welcome To Product Item Page</h1>    
+        </div>
+    )
+}
+
+export default ProductItem;
