ea646b0fbee4d9f6cb68d1a5c4052f26419db4af
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-08 02:39:00 +0500

    Update Route.js
    Removed all code

--- src/Route.js
+++ src/Route.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 const RouteConfig = () => {
     return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route path="/" component={ } />
-                </Switch>
-            </Router>
-        </div>
+        <div></div>
     );
 }
 
