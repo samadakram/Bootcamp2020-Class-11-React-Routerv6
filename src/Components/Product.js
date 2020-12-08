fb77c525e5faa5dc48081f9c7a3783bb3d8797ea
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-09 02:29:19 +0500

    Update Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -4,10 +4,15 @@ import Shoes from '../../src/shoes.json';
 const Product = () => {
 
     console.log(Shoes);
-    console.log("Keys>",Object.keys(Shoes));
+    // console.log("Keys>",Object.keys(Shoes));
     return (
         <div>
-            <h1>Welcome To Product Page</h1>    
+            <h1>Welcome To Product Page</h1>
+            {Object.keys(Shoes).map(keyName => {
+                return (<div>
+                    <h2 key={keyName}>{keyName}</h2>
+                </div>)
+            })}
         </div>
     )
 }
