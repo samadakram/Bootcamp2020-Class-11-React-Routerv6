1cfaadababbc1b9741488839cadfa432e58fa30d
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-09 02:24:40 +0500

    Update Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -4,6 +4,7 @@ import Shoes from '../../src/shoes.json';
 const Product = () => {
 
     console.log(Shoes);
+    console.log("Keys>",Object.keys(Shoes));
     return (
         <div>
             <h1>Welcome To Product Page</h1>    
