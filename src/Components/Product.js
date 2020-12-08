326c03203ea22577ca5b0934ec69c990efaf2697
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-09 02:22:14 +0500

    Update Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import Shoes from '../../src/shoes.json';
 
 const Product = () => {
+
+    console.log(Shoes);
     return (
         <div>
             <h1>Welcome To Product Page</h1>    
