676d3562a391655028d016cda12c986ce98069f5
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-10 03:13:36 +0500

    Update ProductItem.js

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductItem = () => {
+
+    let { slug } = useParams();
+
     return (
         <div>
-            <h1>Welcome To Product Item Page</h1>    
+            <h1>Welcome To Product Item Page</h1>
         </div>
     )
 }
